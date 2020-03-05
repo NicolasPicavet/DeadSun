91220edba3bb6b2970e55e6ae89612d51c925cef
Nicolas Picavet <picavet.nicolas@gmail.com>
2020-03-05 23:04:39 +0100

    Add a title scene

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)
+            || Input.GetKeyDown(KeyCode.RightArrow)
+            || Input.GetKeyDown(KeyCode.DownArrow)
+            || Input.GetKeyDown(KeyCode.LeftArrow))
+            SceneManager.LoadScene("MainScene");
+    }
+}
