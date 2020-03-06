db93e0c0f6e4dc11a36527498ea54c36392db067
Nicolas Picavet <picavet.nicolas@gmail.com>
2020-03-06 17:17:45 +0100

    Add version number to title screen

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour {
+
+    void Start() {
+        GameObject.Find("Version").GetComponent<Text>().text = Application.version;
+    }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)
