43394d8c9e24e75191291139ef46f24a8a317479
Nicolas Picavet <picavet.nicolas@gmail.com>
2020-03-06 21:02:48 +0100

    Create post build editor script Copy changelog to build folder

---
+++ Assets/Scripts/EditorBuild.cs
@@ -0,0 +1,16 @@
+﻿ #if UNITY_EDITOR
+ 
+    using UnityEditor;
+    using UnityEditor.Build;
+    using UnityEditor.Build.Reporting;
+
+    class EditorBuild : IPostprocessBuildWithReport {
+
+        public int callbackOrder { get { return 0; } }
+
+        public void OnPostprocessBuild(BuildReport repoort) {
+            FileUtil.ReplaceFile("changelog.txt", "../Build/changelog.txt");
+        }
+    }
+ 
+ #endif
